662a99106e80eef0faa60b2315526d98d109cf96
RefinedWiki Build Agent <bamboo@refinedwiki.com>
2025-12-22 13:00:53 +0100

    testing client build

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT} hel`);
 });
 
